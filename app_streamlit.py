7a6763752ec7f100319efb13431587d5243b1902
Mo Yang <mo_yang@foxmail.com>
2020-12-10 15:31:56 +0100

    change app

--- app_streamlit.py
+++ app_streamlit.py
@@ -5,86 +5,101 @@ from pkg.contentBasedRec import ContentRecommender
 import joblib
 from PIL import Image
 
+sections = st.sidebar.selectbox("choose the section", ["About Us", "Introduction", "Demo"])
 
-# tmp until the feature is released
-st.set_page_config(
-    page_title="Video Game Fanatic",
-    page_icon="🐍",
-    layout="centered", # wide
-    initial_sidebar_state="auto") # collapsed
-
-
-def get_model():
+#### Cache the elements for prediction
+@st.cache(suppress_st_warning=True)
+def load_model():
+    model = joblib.load(f"NaiveBayes.joblib")
     return model
 
-
-def main():
-
-    sections = st.sidebar.selectbox("choose the section", ["About Us", "Introduction", "Demo"])
-
-    if sections == "About Us":
-        col1, col2, col3 = st.beta_columns(3)
-
-        img = Image.open("images/1.jpg")
-        col1.header("Matthieu")
-        col1.image(img, width = 200)
-
-        img = Image.open("images/2.jpg")
-        col2.header("Nicola")
-        col2.image(img, width = 200)
-
-        img = Image.open("images/3.jpg")
-        col3.header("Mo")
-        col3.image(img, width = 200)
-
-        
-
-    if sections == "Introduction":
-        model_name = st.selectbox("Which Model do We Use?", ["NaiveBayes", "Logistic"])
-        model = joblib.load('NaiveBayes.joblib')
-        # st.smage(img, caption = "Learning Curve")
-        if st.button("Show Parameters of the Model"):
-            st.code(model.get_params())
+@st.cache(suppress_st_warning=True)
+def get_games():
+    game_list = get_game_lst()
+    return game_list
 
 
-    if sections == "Demo":
-
-        st.header("Tell us What do you feel about your last Purchase!")
-        st.markdown("### Exciting ###")
-
-        ## input from user
-        user_name = st.text_input("Please Enter Your Name", "Type here...")
-        if st.button("submit"):
-            result = user_name.title()
-            st.success(f"Thank You {result}")
-        
-        game_list = get_game_lst()
-        game_name = st.selectbox("What is the Game you purchased?", game_list)
-
-        rec = ContentRecommender(example = game_name)
-        recommendation = rec.get_recommendation()
-
- 
-
-        message = st.text_area("Enter Your Message", "I Love this game!")
-        message = preprocess(message)
-
-       
-        model_name = st.selectbox("Which Model?", ["NaiveBayes", "Logistic"])
-        
-        
-        model = joblib.load(f"{model_name}.joblib")
-
-        pred = model.predict([message])
+### Easter_egg
+@st.cache(suppress_st_warning=True)
+def easter_egg(inputt):
+    names = ["benjamin martin", "carlotta lukarsch", "carly", "chongqing wang", "harrison", "lars", "leo bierbach", "marnix sliepenbeek", "nour din saffour", "tjerk kostelijk"]
+    for name in names:
+        if (inputt.lower() in name) or (name in inputt.lower()):
+            string = "This Review Has not been taken into account as " + inputt + " is retarded"
+            return string.capitalize()
+        else:
+            string = "Thank you " + inputt.Capitalize()
+            return string
 
 
-        if st.button("Submit the Review"):
-            reply = autoreply(review_score = pred, name=user_name, product=game_name, recommandation = recommendation)
-            if pred == 1:
-                st.success(reply)
-            else:
-                st.warning(reply)
+if sections == "About Us":
+    col1, col2, col3 = st.beta_columns(3)
 
-if __name__ == "__main__":
-    #df = read_data()
-    main()
+    img1 = Image.open("images/1.jpg")
+    col1.header("Matthieu")
+    col1.image(img1, width = 200)
+
+    img2 = Image.open("images/2.jpg")
+    col2.header("Nicola")
+    col2.image(img2, width = 200)
+
+    img3 = Image.open("images/3.jpg")
+    col3.header("Mo")
+    col3.image(img3, width = 200)
+
+
+
+
+
+if sections == "Demo":
+
+    st.markdown("## Tell us What do you think about your last Purchase!")
+
+    ########################
+    ## User put in his name
+    ########################
+    user_name = st.text_input("Please Enter Your Name", "Type here...")
+    if st.button("submit"):
+        st.success(easter_egg(user_name))
+
+    ########################
+    ## User put in his name
+    ########################
+    game_list = get_games()
+    game_name = st.selectbox("What is the Game you purchased?", game_list)   
+
+
+    ########################
+    ## User put in his name
+    ########################
+    message = st.text_area("Enter Your Message", "I Love this game!")
+    message = preprocess(message)
+    model = load_model()
+    pred = model.predict([message])
+   
+
+    if st.button("Submit the Review"):
+        reply = autoreply(review_score = pred, name=user_name, product=game_name, recommandation = recommendation)
+        if pred == 1:
+            st.success(reply)
+        else:
+            st.warning(reply)
+
+    rec = ContentRecommender(example = game_name)
+    recommendations = rec.get_recommendation()
+    for i in recommendations:
+        st.markdown(i)
+
+
+
+
+
+if sections == "Introduction":
+    # model_name = st.selectbox("Which Model do We Use?", ["NaiveBayes", "Logistic"])
+    # model = joblib.load('NaiveBayes.joblib')
+    # st.code(model)
+    # # st.smage(img, caption = "Learning Curve")
+    # if st.button("Show Parameters of the Model"):
+    #     st.code(model.get_params())
+    pass
+
